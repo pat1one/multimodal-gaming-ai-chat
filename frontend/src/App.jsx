9c460b0f6d0f498664b8c5ff69751f89f943a7ad
Platon <drow.battle.2015@gmail.com>
2025-07-30 01:06:20 +0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import ChatInterface from "./components/ChatInterface";
+
+const App = () => (
+  <div>
+    <h1>Multimodal Gaming AI Chat</h1>
+    <ChatInterface />
+  </div>
+);
+
+export default App;
